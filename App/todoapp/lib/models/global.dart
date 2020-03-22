3dca4cb1002d7dafaa6568374612df211b708499
unknown <devSharjeel>
2020-03-22 10:00:19 -0400

    added models folder and global file inside that. also, modified Appbar and homepage title.

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final Color darkGreyColor = new Color(0xFF212128);
+
+final TextStyle NotesTileStyle = new TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 40.0,
+  color: Colors.white,
+  fontFamily: 'Demibold'
+);
